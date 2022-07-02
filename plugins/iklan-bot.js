bf785d837b1a4435dd9a5c07bc3cc77fc142c331
EpahBot <101261029+EpahBot@users.noreply.github.com>
2022-07-02 19:07:43 +0800

    Update iklan-bot.js

--- plugins/iklan-bot.js
+++ plugins/iklan-bot.js
@@ -4,16 +4,52 @@ let handler = async(m, { conn }) => {
     let kontol = `
 ╭─────[ *STORE BOT* ]─────✧
 ┴
-│ *STORE ArullBotzMD*
-┬
-╰──────────···`.trim()
+│❏  1 Minggu : RM 5.00
+│❏  1 Bulan : RM 10.00
+╰────
+╭─ [ *_KELEBIHAN BOT_* ]
+│❏  On 24 Jam
+│❏  Fitur Lengkap
+╰────
+╭─[ *_Keuntungan_* ]
+│❏  Antilink ( Auto Kick )
+│❏  Welcome Image
+│❏  Leave Image
+│❏  Cocok Buat Jaga Grup
+│❏  Dan Ratusan Fitur Lainnya
+╰────
+╭─[ *_MINAT CHAT_* ]
+│❏  🪀 wa.me/60109102984
+╰────
+
+╭──「 *_JADIBOT + OWNER_* 」
+│❏   RM 20.00
+╰────
+╭─ [ *_FASILITAS_* ]
+│❏  Cuma Scan QR Doang
+│❏  Fitur Bot Nya Banyak
+│❏  Banyak Fitur Langka
+╰────
+╭─[ *_KEUNTUNGAN_* ]
+│❏  Bisa Open Sewabot
+│❏  Nama Bot + Owner
+│❏  Bisa Dapat Cuan
+╰────
+╭─[ *_MINAT CHAT_* ]
+│❏  🪀 wa.me/60109102984
+╰──── 
+
+╭─[ *_PEMBAYARAN_*  ]
+│❏  Via Digi 0109102984
+╰────────────
+©Khairil春`.trim()
   const button = {
-        buttonText: '🛒STORE ArullBotzMD 🛒',
+        buttonText: '🛒STORE BebanBot天 🛒',
         description: kontol,
-        sections:  [{title: "STORE ArullBotzMD", rows: [
+        sections:  [{title: "STORE BebanBot天", rows: [
         {title: 'Menu utama', description: "Kembali ke Menu Utama", rowId:".?"},
         {title: 'Sewa satbotsss', description: "Sewa bot dengan memasukkan bot ke grup kamu", rowId:",
-        {title: 'RULES', description: "rules ArullBotzMD", rowId:".rules"},
+        {title: 'RULES', description: "rules BebanBot天", rowId:".rules"},
        ] }],
         listType: 1
        }
