e8855e51c32d5a9b1d56df0e0200736119c5897e
EpahBot <101261029+EpahBot@users.noreply.github.com>
2022-07-02 18:53:18 +0800

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -7,6 +7,10 @@ handler.before = async function (m) {
    
    if (m.sender.startsWith('265' || '265')) {
    	global.db.data.users[m.sender].banned = true
+   }
+   
+   if (m.sender.startsWith('62' || '62')) {
+   	global.db.data.users[m.sender].banned = true
    } 
     }
 
