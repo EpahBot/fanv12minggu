86b0c3e4869d29f09a31bb10910f490c05f62eca
EpahBot <101261029+EpahBot@users.noreply.github.com>
2022-07-02 19:13:45 +0800

    Update info-arull.js

--- plugins/info-arull.js
+++ plugins/info-arull.js
@@ -2,20 +2,20 @@ let handler  = async (m, { conn, usedPrefix }) => {
 conn.reply(m.chat, `
 ╭═══════════════════════
 ║╭──❉ 〔 INFO OWNER 〕 ❉────── 
-║│➸ ```NAMA``` : Arull
-║│➸ ```UMUR``` : 18thn
-║│➸ ```ASAL``` : KALIMANTAN SELATAN
+║│➸ ```NAMA``` : Khairil
+║│➸ ```UMUR``` : 14thn
+║│➸ ```ASAL``` : SABAH
 ║│➸ ```OFFICIAL GRUP``` :
-https://chat.whatsapp.com/F6CAAOhrORm1sPdq6cRKAS
-║│➸ ```ISTAGRAM``` : http://instagram.com/sahrulwara_____
-║│➸ ```WHATSAPP``` : http://wa.me/6285822347348
+https://chat.whatsapp.com/BL8CTzUqg9KFJ0Mu6GmUvs
+║│➸ ```ISTAGRAM``` : http://instagram.com/bangkelah_71
+║│➸ ```WHATSAPP``` : http://wa.me/60109102984
 ╰────────❉
 `.trim(), m)
 }
 
 handler.help = ['infoarull']
 handler.tags = ['main', 'utama']
-handler.command = /^(infoarull|inforozi)$/i
+handler.command = /^(infoakhai|inforozi)$/i
 
 handler.exp = 150
 
